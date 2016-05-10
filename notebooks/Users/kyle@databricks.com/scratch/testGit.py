b465f8d56a62fbbbf773f7bfdd45282a00a787ee
Kyle <ktpistor@gmail.com>
2016-05-10 13:04:10 -0700

    First commit

--- notebooks/Users/kyle@databricks.com/scratch/testGit.py
+++ notebooks/Users/kyle@databricks.com/scratch/testGit.py
@@ -1,5 +1,5 @@
 # Databricks notebook source exported at Tue, 10 May 2016 19:53:30 UTC
-df = sqlContext.sql("select * from diamonds limit 10")
+df = sqlContext.sql("select * from diamonds limit 10").count()
 
 # COMMAND ----------
 
